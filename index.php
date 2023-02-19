8f8d97cab4bb8fb6d693f4db2d50ae1e28fd15eb
Uddhab Haldar <uddhab13@gmail.com>
2023-02-20 02:45:42 +0530

    Added some styling and optimized code

--- index.php
+++ index.php
@@ -1,65 +1,63 @@
 <?php
-// iTunes top podcasts
-$itunes_endpoint = "https://itunes.apple.com/us/rss/toppodcasts/limit=100/json";
-$itunes_response = file_get_contents($itunes_endpoint);
-$itunes_data = json_decode($itunes_response, true);
+// Set the number of podcasts to display
+$numOfPodcasts = 100;
 
-// Spotify top podcasts
-$spotify_endpoint = "https://podcastcharts.byspotify.com/api/charts/top?region=us";
-$spotify_response = file_get_contents($spotify_endpoint);
-$spotify_data = json_decode($spotify_response, true);
+// Get iTunes top podcasts
+$itunesEndpoint = "https://itunes.apple.com/us/rss/toppodcasts/limit=$numOfPodcasts/json";
+$itunesResponse = file_get_contents($itunesEndpoint);
+$itunesData = json_decode($itunesResponse, true);
 
-function getSpotifyShowUrl($showUri) {
-  $showId = str_replace('spotify:show:', '', $showUri);
-  return "https://open.spotify.com/show/$showId";
+// Get Spotify top podcasts
+$spotifyEndpoint = "https://podcastcharts.byspotify.com/api/charts/top?region=us";
+$spotifyResponse = file_get_contents($spotifyEndpoint);
+$spotifyData = json_decode($spotifyResponse, true);
+
+// Function to get Spotify show URL
+function getSpotifyShowUrl($showUri)
+{
+    return "https://open.spotify.com/show/" . str_replace('spotify:show:', '', $showUri);
 }
-?>
 
+// Output HTML
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en-us">
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css" integrity="sha512-wpPYUAdjBVSE4KJnH1VR1HeZfpl1ub8YT/NKx4PuQ5NmX2tKuGu6U/JRp5y+Y8XG2tV+wKQpNHVUX03MfMFn9Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top <?php echo $numOfPodcasts ?> Podcasts from Apple and Spotify</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h2>Top 100 Apple Podcasts</h2>
-    <div class="grid-container">
-        <?php $counter = 1; ?>
-        <?php foreach($itunes_data['feed']['entry'] as $entry): ?>
-            <?php
-            $image_url = $entry['im:image'][2]['label'];
+    <h2>Top <?php echo $numOfPodcasts ?> Apple Podcasts</h2>
+    <div class="grid-container apple-podcasts">
+        <?php foreach ($itunesData['feed']['entry'] as $key => $entry) :
+            if ($key >= $numOfPodcasts) break;
+            $imageUrl = $entry['im:image'][2]['label'];
             $title = $entry['im:name']['label'];
             $link = $entry['link']['attributes']['href'];
-            ?>
-            <div class="grid-item">
-                <a href="<?php echo $link ?>" target="_blank">
-                    <div><?php echo $counter ?></div>
-                    <img src="<?php echo $image_url ?>">
-                    <div><?php echo $title ?></div>
-                </a>
-            </div>
-            <?php $counter++; ?>
+        ?>
+            <a class="grid-item" href="<?php echo $link ?>" target="_blank">
+                <div class="counter"><?php echo $key + 1 ?></div>
+                <img class="podcast-image" src="<?php echo $imageUrl ?>" alt="<?php echo $title ?>" loading="lazy">
+                <div class="podcast-title"><?php echo $title ?></div>
+            </a>
         <?php endforeach; ?>
     </div>
-
-    <h2>Top 100 Spotify Podcasts</h2>
-    <div class="grid-container">
-        <?php $counter = 1; ?>
-        <?php foreach($spotify_data as $entry): ?>
-            <?php
-            $image_url = $entry['showImageUrl'];
+    <h2>Top <?php echo $numOfPodcasts ?> Spotify Podcasts</h2>
+    <div class="grid-container spotify-podcasts">
+        <?php foreach ($spotifyData as $key => $entry) :
+            if ($key >= $numOfPodcasts) break;
+            $imageUrl = $entry['showImageUrl'];
             $title = $entry['showName'];
             $link = getSpotifyShowUrl($entry['showUri']);
-            ?>
-            <div class="grid-item">
-                <a href="<?php echo $link ?>" target="_blank">
-                    <div><?php echo $counter ?></div>
-                    <img src="<?php echo $image_url ?>">
-                    <div><?php echo $title ?></div>
-                </a>
-            </div>
-            <?php $counter++; ?>
-            <?php if ($counter > 100) break; ?>
+        ?>
+            <a class="grid-item" href="<?php echo $link ?>" target="_blank">
+                <div class="counter"><?php echo $key + 1 ?></div>
+                <img class="podcast-image" src="<?php echo $imageUrl ?>" alt="<?php echo $title ?>" loading="lazy">
+                <div class="podcast-title"><?php echo $title ?></div>
+            </a>
         <?php endforeach; ?>
     </div>
 </body>
