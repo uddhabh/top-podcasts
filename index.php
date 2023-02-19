77fb7e80bd43430e6673311c3009f18faebbb764
Uddhab Haldar <uddhab13@gmail.com>
2023-02-19 17:31:59 +0530

    Top 100 Spotify Podcasts Added

--- index.php
+++ index.php
@@ -1,39 +1,31 @@
 <?php
-$endpoint = "https://itunes.apple.com/us/rss/toppodcasts/limit=100/json";
-$response = file_get_contents($endpoint);
-$data = json_decode($response, true);
+// iTunes top podcasts
+$itunes_endpoint = "https://itunes.apple.com/us/rss/toppodcasts/limit=100/json";
+$itunes_response = file_get_contents($itunes_endpoint);
+$itunes_data = json_decode($itunes_response, true);
+
+// Spotify top podcasts
+$spotify_endpoint = "https://podcastcharts.byspotify.com/api/charts/top?region=us";
+$spotify_response = file_get_contents($spotify_endpoint);
+$spotify_data = json_decode($spotify_response, true);
+
+function getSpotifyShowUrl($showUri) {
+  $showId = str_replace('spotify:show:', '', $showUri);
+  return "https://open.spotify.com/show/$showId";
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <style>
-        .grid-container {
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-            grid-gap: 20px;
-        }
-
-        .grid-item {
-            text-align: center;
-        }
-
-        .grid-item img {
-            display: block;
-            margin: 0 auto;
-            max-width: 100%;
-        }
-
-        .grid-item a {
-            color: black;
-            text-decoration: none;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css" integrity="sha512-wpPYUAdjBVSE4KJnH1VR1HeZfpl1ub8YT/NKx4PuQ5NmX2tKuGu6U/JRp5y+Y8XG2tV+wKQpNHVUX03MfMFn9Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <h2>Top 100 Apple Podcasts</h2>
     <div class="grid-container">
         <?php $counter = 1; ?>
-        <?php foreach($data['feed']['entry'] as $entry): ?>
+        <?php foreach($itunes_data['feed']['entry'] as $entry): ?>
             <?php
             $image_url = $entry['im:image'][2]['label'];
             $title = $entry['im:name']['label'];
@@ -41,12 +33,33 @@ $data = json_decode($response, true);
             ?>
             <div class="grid-item">
                 <a href="<?php echo $link ?>" target="_blank">
-                    <div><?php echo $counter . ". " ?></div>
+                    <div><?php echo $counter ?></div>
                     <img src="<?php echo $image_url ?>">
                     <div><?php echo $title ?></div>
                 </a>
             </div>
             <?php $counter++; ?>
+        <?php endforeach; ?>
+    </div>
+
+    <h2>Top 100 Spotify Podcasts</h2>
+    <div class="grid-container">
+        <?php $counter = 1; ?>
+        <?php foreach($spotify_data as $entry): ?>
+            <?php
+            $image_url = $entry['showImageUrl'];
+            $title = $entry['showName'];
+            $link = getSpotifyShowUrl($entry['showUri']);
+            ?>
+            <div class="grid-item">
+                <a href="<?php echo $link ?>" target="_blank">
+                    <div><?php echo $counter ?></div>
+                    <img src="<?php echo $image_url ?>">
+                    <div><?php echo $title ?></div>
+                </a>
+            </div>
+            <?php $counter++; ?>
+            <?php if ($counter > 100) break; ?>
         <?php endforeach; ?>
     </div>
 </body>
